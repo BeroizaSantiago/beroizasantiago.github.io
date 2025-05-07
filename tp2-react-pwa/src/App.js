d9260e6b240bae921de66715cc3b5c517ae33cd0
beroizasantiago <93729026+BeroizaSantiago@users.noreply.github.com>
2025-05-07 16:29:38 -0300

    Add files via upload

---
+++ tp2-react-pwa/src/App.js
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import ROUTES from './const/Routes'
+import Home from './pages/Home/Home'
+import Detalles from './pages/Detalles/Detalles';
+import Favoritos from './pages/Favoritos/Favoritos'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={ROUTES.home} element={<Home />} />
+        <Route path={ROUTES.detalles} element={<Detalles />} />
+        <Route path={ROUTES.favoritos} element={<Favoritos />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App
---
+++ tp2-react-pwa/src/pages/Favoritos/Favoritos.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+import TarjetaReceta from "../../components/TarjetaReceta/TarjetaReceta";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+import { useTranslation } from "react-i18next";
+
+const Favoritos = () => {
+  const [favoritos, setFavoritos] = useState([]);
+  const { t } = useTranslation();
+
+  useEffect(() => {
+    const listaFavs = JSON.parse(localStorage.getItem("favoritos")) || [];
+    setFavoritos(listaFavs);
+  }, []);
+
+  const eliminarFavorito = (id) => {
+    const nuevosFavoritos = favoritos.filter((receta) => receta.id !== id);
+    setFavoritos(nuevosFavoritos);
+    localStorage.setItem("favoritos", JSON.stringify(nuevosFavoritos));
+  };
+
+  return (
+    <div className="bg-gray-100 min-h-screen">
+      <Header />
+      <div className="p-6 max-w-7xl mx-auto">
+        <h1 className="text-4xl font-bold text-center mb-6 text-gray-700 font-dancing">{t('favRecipes')}</h1>
+
+        {favoritos.length > 0 ? (
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+            {favoritos.map((receta) => (
+              <div key={receta.id} className="relative">
+                <TarjetaReceta receta={receta} onToggleFavorito={() => eliminarFavorito(receta.id)} />
+
+              </div>
+            ))}
+          </div>
+        ) : (
+          <p className="text-center text-lg text-gray-500">{t('noFavorites')}</p>
+        )}
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Favoritos;
